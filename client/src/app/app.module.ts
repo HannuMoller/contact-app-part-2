8d26fd99e587976edf172ca7595cf91a5d3a8590
HannuMoller <moller.hannu@gmail.com>
2017-04-04 14:34:57 +0300

    Flex-Layout installed and imported

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
 import { ContactListComponent } from './contact/contact-list/contact-list/contact-list.component';
 import { ContactListItemComponent } from './contact/contact-list-item/contact-list-item/contact-list-item.component';
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
@@ -17,8 +17,8 @@ import { ContactListItemComponent } from './contact/contact-list-item/contact-li
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
-//    BrowserAnimationsModule
+    HttpModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
